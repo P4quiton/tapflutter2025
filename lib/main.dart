e318160c098390e6b8efd35a1000c0ab4b1dcd00
Ruben Torres Frias <isctorres@gmail.com>
2025-04-02 11:06:22 -0600

    creación del repositorio

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:tap2025/screens/contador_screen.dart';
+import 'package:tap2025/screens/login_screen.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      home: LoginScreen(),
+    );
+  }
+}
